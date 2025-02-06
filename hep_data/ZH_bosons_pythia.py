741c08f35138822dddfa1100c610593873fef953
Andres Ramirez <stormchildremix@gmail.com>
2025-02-06 12:35:50 -0600

    ZH bosons OK

--- hep_data/ZH_bosons_pythia.py
+++ hep_data/ZH_bosons_pythia.py
@@ -7,8 +7,6 @@ import math
 
 pythia=pythia8.Pythia()
 pythia.readString("HiggsSM:ffbar2HZ = on")  #  ZH production via qqbar -> ZH
-pythia.readString("36:onIfMatch = 25 23")
-# pythia.readString("Beams:eCM = 100000")
 
 # Z decays
 pythia.readString("23:onMode = off")
@@ -39,7 +37,7 @@ antimuon_py = []
 antimuon_pz = []
 antimuon_e = []
 
-# listas para b quarks asociados a electrones
+# electrones bquarks
 bquark_px_e = []
 bquark_py_e = []
 bquark_pz_e = []
@@ -49,7 +47,7 @@ anti_bquark_py_e = []
 anti_bquark_pz_e = []
 anti_bquark_e_e = []
 
-#listas para b quarks asociados a muones
+# muons bquarks
 bquark_px_m = []
 bquark_py_m = []
 bquark_pz_m = []
@@ -66,7 +64,6 @@ eta=[]
 contador=0
 
 
-
 def find_ancestor(event, particle):
     particle_id = particle.id()
     current_idx = particle.mother1()
@@ -74,11 +71,8 @@ def find_ancestor(event, particle):
         mother1_idx = event[current_idx].mother1()
         mother1 = event[mother1_idx]
         mother1_id = mother1.id()
-
         if mother1_id != particle_id and abs(mother1_id) in [1,2,3,4,5,6]:
-            # print(mother1_id, mother1_idx, particle_id)
-            return mother1_id, mother1_idx
-
+            return (mother1_id, mother1_idx) # returns tuple
         current_idx = mother1_idx
     
 
@@ -88,120 +82,105 @@ for j in range(num_events):
     if not pythia.next():
         continue
     
-    evento = pythia.event
-
-    print("********************************************************")
+    event = pythia.event
     mother_fermion_Z = None
     mother_fermion_Z = None
 
-    for particle in evento:
+    for particle in event:
             
         if particle.id() == 23: # Z boson
             Z_boson = particle
-            daughter1_Z = evento[Z_boson.daughter1()]
-            daughter2_Z = evento[Z_boson.daughter2()]
+            daughter1_Z = event[Z_boson.daughter1()]
+            daughter2_Z = event[Z_boson.daughter2()]
 
-            if abs(daughter1_Z.id()) == 11 and abs(daughter2_Z.id()) == 11:
-                mother_fermion_Z = find_ancestor(evento, Z_boson)
+            if abs(daughter1_Z.id()) == 11 and abs(daughter2_Z.id()) == 11:        
+                mother_fermion_Z = find_ancestor(event, Z_boson)
                 Zboson_final = Z_boson
 
-        if particle.id() == 25: # Z boson
+        if particle.id() == 25: # H boson
             H_boson = particle
-            daughter1_H = evento[H_boson.daughter1()]
-            daughter2_H = evento[H_boson.daughter2()]
+            daughter1_H = event[H_boson.daughter1()]
+            daughter2_H = event[H_boson.daughter2()]
 
             if abs(daughter1_H.id()) == 5 and abs(daughter2_H.id()) == 5:
-                mother_fermion_H = find_ancestor(evento, H_boson)
+                mother_fermion_H = find_ancestor(event, H_boson)
                 Hboson_final = H_boson
 
-    if mother_fermion_Z == mother_fermion_H and mother_fermion_Z != None:
-        print(evento[Zboson_final.daughter1()].id(), evento[Zboson_final.daughter2()].id(), "ZBoson")
-        print(evento[Hboson_final.daughter1()].id(), evento[Hboson_final.daughter2()].id(), "HBoson")   
-            
-        '''
-        if particle.id() == 25 or False: # Z boson mother
+    if mother_fermion_Z == mother_fermion_H and mother_fermion_Z != None: # check that Z and H come from same mother
 
-            
-            ancestor_index_H = find_ancestor(evento, mother_pos)
-            #print(ancestor_index_H)
+        Zboson_daughter1 = event[Zboson_final.daughter1()]
+        Zboson_daughter2 = event[Zboson_final.daughter2()]
 
+        if Zboson_daughter1.id()==11:
+            electron = Zboson_daughter1
+        elif Zboson_daughter2.id()==11:
+            electron = Zboson_daughter2
 
+        if Zboson_daughter1.id()==-11:
+            positron = Zboson_daughter1
+        elif Zboson_daughter2.id()==-11:
+            positron = Zboson_daughter2
 
-     
-        if particle.id() == 23 or particle.id() == 25 and False: # Z boson mother
-            print(particle.id(), particle.daughter1(), evento[particle.daughter1()].id(), evento[particle.mother1()].id(), j)
+        Hboson_daughter1 = event[Hboson_final.daughter1()]
+        Hboson_daughter2 = event[Hboson_final.daughter2()]
 
+        # electron bottoms
+        if Hboson_daughter1.id()==5:
+            quark_b = Hboson_daughter1
+        elif Hboson_daughter2.id()==5:
+            quark_b= Hboson_daughter2
+                    
+        if Hboson_daughter1.id()==-5:
+            antiquark_b = Hboson_daughter1
+        elif Hboson_daughter2.id()==-5:
+            antiquark_b = Hboson_daughter2
+    
+        # electron kinematics
+        electron_px.append(electron.px())
+        electron_py.append(electron.py())
+        electron_pz.append(electron.pz())
+        electron_e.append(electron.e())
+                    
+        # positron kinematics 
+        positron_px.append(positron.px())
+        positron_py.append(positron.py())
+        positron_pz.append(positron.pz())
+        positron_e.append(positron.e())
+                    
+        # bquark - electron kinematics
+        bquark_px_e.append(quark_b.px())
+        bquark_py_e.append(quark_b.py()) 
+        bquark_pz_e.append(quark_b.pz()) 
+        bquark_e_e.append(quark_b.e())
+                    
+        # antibquark - electron kinematics
+        anti_bquark_px_e.append(antiquark_b.px()) 
+        anti_bquark_py_e.append(antiquark_b.py()) 
+        anti_bquark_pz_e.append(antiquark_b.pz())
+        anti_bquark_e_e.append(antiquark_b.e())
 
-           
-            daughter1_Z = evento[particle.daughter1()]
-            daughter2_Z = evento[particle.daughter2()]
-            
-            if (abs(daughter1_Z.id()) == 23 or abs(daughter1_Z.id()) == 25) and (abs(daughter2_Z.id()) == 23 or abs(daughter2_Z.id()) == 25) :
-                
-                if abs(daughter1_Z.id())==23:
-                    Zboson_Z = daughter1_Z
-                elif abs(daughter2_Z.id())==23:
-                    Zboson_Z = daughter2_Z
-                    
-                if abs(daughter1_Z.id())==25:
-                    Hboson_Z = daughter1_Z
-                elif abs(daughter2_Z.id())==25:
-                    Hboson_Z = daughter2_Z
-                
-                # Higgs daughters
-                daughter1_H = evento[Hboson_Z.daughter1()]
-                daughter2_H = evento[Hboson_Z.daughter2()]
-                Hdecay_ok = False
-                if abs(daughter1_H.id()) == 5 and abs(daughter2_H.id()) == 5:
-                    Hdecay_ok = True
-                # Z daughters
-                daughter1_Z = evento[Zboson_Z.daughter1()]
-                daughter2_Z = evento[Zboson_Z.daughter2()]
-                
-                if abs(daughter1_Z.id()) == 11 and abs(daughter2_Z.id()) == 11 and Hdecay_ok:
-                   #acomodar electrones
-                    if daughter1_Z.id()==11:
-                        electron = daughter1_Z
-                    elif daughter2_Z.id()==11:
-                        electron = daughter2_Z
-                    
-                    if daughter1_Z.id()==-11:
-                        positron = daughter1_Z
-                    elif daughter2_Z.id()==-11:
-                        positron = daughter2_Z
-                    
-                    #bottoms para electrones
-                    if daughter1_H.id()==5:
-                        quark_b = daughter1_H
-                    elif daughter2_H.id()==5:
-                        quark_b= daughter2_H
-                    
-                    if daughter1_H.id()==-5:
-                        antiquark_b = daughter1_H
-                    elif daughter2_H.id()==-5:
-                        antiquark_b = daughter2_H
-                    
-                if abs(daughter1_Z.id()) == 13 and abs(daughter2_Z.id()) == 13 and Hdecay_ok:
-                   #acomodar muones
-                    if daughter1_Z.id()==13:
-                        muon = daughter1_Z
-                    elif daughter2_Z.id()==13:
-                        muon= daughter2_Z
-                    
-                    if daughter1_Z.id()==-13:
-                        antimuon = daughter1_Z
-                    elif daughter2_Z.id()==-13:
-                        antimuon = daughter2_Z
-                    
-                    #bottoms para muones
-                    if daughter1_H.id()==5:
-                        quark_b = daughter1_H
-                    elif daughter2_H.id()==5:
-                        quark_b= daughter2_H
-                    
-                    if daughter1_H.id()==-5:
-                        antiquark_b = daughter1_H
-                    elif daughter2_H.id()==-5:
-                        antiquark_b = daughter2_H
-                        '''
-                    
+# end event generation
+pythia.stat()
+
+# electron data frame
+df_electrones = pd.DataFrame({
+    'electron_px': electron_px,
+    'electron_py': electron_py,
+    'electron_pz': electron_pz,
+    'electron_e': electron_e,
+    'positron_px': positron_px,
+    'positron_py': positron_py,
+    'positron_pz': positron_pz,
+    'positron_e': positron_e,
+    'bquark_px_e': bquark_px_e,
+    'bquark_py_e': bquark_py_e,
+    'bquark_pz_e': bquark_pz_e,
+    'bquark_e_e': bquark_e_e,
+    'anti_bquark_px_e': anti_bquark_px_e,
+    'anti_bquark_py_e': anti_bquark_py_e,
+    'anti_bquark_pz_e': anti_bquark_pz_e,
+    'anti_bquark_e_e': anti_bquark_e_e,
+})
+
+# save as csv
+df_electrones.to_csv('data_zh_electrons.csv', index=False) 
