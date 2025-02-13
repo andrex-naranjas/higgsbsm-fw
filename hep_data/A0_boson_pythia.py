3f14406cae048bd21974a24961ca5233cc96a4c4
Oscar González <oscar.gonzalez.cbtis49@gmail.com>
2025-02-13 17:24:59 -0600

    actualized code for generating data

--- hep_data/A0_boson_pythia.py
+++ hep_data/A0_boson_pythia.py
@@ -63,12 +63,6 @@ anti_bquark_py_m = []
 anti_bquark_pz_m = []
 anti_bquark_e_m = []
 
-Z_px = []
-Z_px_m = []
-cos_theta_hist =[]
-eta=[]
-contador=0
-
 num_events=10000
 for j in range(num_events):
     
@@ -76,11 +70,7 @@ for j in range(num_events):
         continue
     
     evento = pythia.event
-    for particle in evento:
-        if particle.id() == 111:
-          #  cos_theta = (particle.pz())/np.sqrt((pow(particle.px(),2) + pow(particle.py(),2) + pow(particle.pz(),2)))
-            cos_theta_hist.append(particle.phi())
-            
+    for particle in evento:            
         mother=particle.mother1()
         madre_id=evento[mother].id()
         mother2=particle.mother2()
@@ -160,19 +150,7 @@ for j in range(num_events):
                     anti_bquark_py_e.append(antiquark_b.py()) 
                     anti_bquark_pz_e.append(antiquark_b.pz())
                     anti_bquark_e_e.append(antiquark_b.e())
-                    
-                    #cos_theta = (Zboson_A0.pz())/(Zboson_A0.px() + Zboson_A0.py() + Zboson_A0.pz())
-                    #cos_theta = (Zboson_A0.pz())/np.sqrt((pow(Zboson_A0.px(),2) + pow(Zboson_A0.py(),2) + pow(Zboson_A0.pz(),2)))
-                    #cos_theta_hist.append(particle.eta())
-                    #cos_theta_hist.append(cos_theta)
-                
-                    Z_px.append(Zboson_A0.px())
-                    
-                 
-                    
-                    #Hmass_squared = pow(quark_b.e() + antiquark_b.e(), 2) - pow(quark_b.px() + antiquark_b.px(), 2) - pow(quark_b.py() + antiquark_b.py(), 2) - pow(quark_b.pz() + antiquark_b.pz(), 2)
-                    #Hmass = math.sqrt(Hmass_squared)
-                    
+
                 if abs(daughter1_Z.id()) == 13 and abs(daughter2_Z.id()) == 13 and Hdecay_ok:
                    #acomodar muones
                     if daughter1_Z.id()==13:
@@ -219,10 +197,7 @@ for j in range(num_events):
                     anti_bquark_py_m.append(antiquark_b.py()) 
                     anti_bquark_pz_m.append(antiquark_b.pz())
                     anti_bquark_e_m.append(antiquark_b.e())
-                    contador+=1
-                    Z_px_m.append(Zboson_A0.px())
 
-print(len(anti_bquark_px_e), len(anti_bquark_px_m))
 # Crear DataFrame para electrones
 df_electrones = pd.DataFrame({
     'electron_px': electron_px,
@@ -233,15 +208,14 @@ df_electrones = pd.DataFrame({
     'positron_py': positron_py,
     'positron_pz': positron_pz,
     'positron_e': positron_e,
-    'bquark_px_e': bquark_px_e,
-    'bquark_py_e': bquark_py_e,
-    'bquark_pz_e': bquark_pz_e,
-    'bquark_e_e': bquark_e_e,
-    'anti_bquark_px_e': anti_bquark_px_e,
-    'anti_bquark_py_e': anti_bquark_py_e,
-    'anti_bquark_pz_e': anti_bquark_pz_e,
-    'anti_bquark_e_e': anti_bquark_e_e,
-    'Z_boson_px_e': Z_px
+    'quark_px_e': bquark_px_e,
+    'quark_py_e': bquark_py_e,
+    'quark_pz_e': bquark_pz_e,
+    'quark_e_e': bquark_e_e,
+    'anti_quark_px_e': anti_bquark_px_e,
+    'anti_quark_py_e': anti_bquark_py_e,
+    'anti_quark_pz_e': anti_bquark_pz_e,
+    'anti_quark_e_e': anti_bquark_e_e
 })
 # Guardar como CSV
 df_electrones.to_csv('dataframe_electrones.csv', index=False) 
@@ -257,23 +231,15 @@ df_muones = pd.DataFrame({
     'antimuon_py': antimuon_py,
     'antimuon_pz': antimuon_pz,
     'antimuon_e': antimuon_e,
-    'bquark_px_m': bquark_px_m,
-    'bquark_py_m': bquark_py_m,
-    'bquark_pz_m': bquark_pz_m,
-    'bquark_e_m': bquark_e_m,
-    'anti_bquark_px_m': anti_bquark_px_m,
-    'anti_bquark_py_m': anti_bquark_py_m,
-    'anti_bquark_pz_m': anti_bquark_pz_m,
-    'anti_bquark_e_m': anti_bquark_e_m,
-    'Z_boson_px_m': Z_px_m
+    'quark_px_m': bquark_px_m,
+    'quark_py_m': bquark_py_m,
+    'quark_pz_m': bquark_pz_m,
+    'quark_e_m': bquark_e_m,
+    'anti_quark_px_m': anti_bquark_px_m,
+    'anti_quark_py_m': anti_bquark_py_m,
+    'anti_quark_pz_m': anti_bquark_pz_m,
+    'anti_quark_e_m': anti_bquark_e_m
 })
 
 # Guardar como CSV
 df_muones.to_csv('dataframe_muones.csv', index=False)
-
-plt.hist(cos_theta_hist, bins=50, range=(-1, 1), alpha=0.75, color='green', label='pi0 Cos theta')
-plt.xlabel("pi0 Cos theta")
-plt.ylabel("Events")
-plt.title("Cosine of theta of pi0")
-plt.legend()
-plt.show()
